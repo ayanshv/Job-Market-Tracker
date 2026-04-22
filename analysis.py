ae9b1f6a8c3ead0747f2c30b49a23efff8f3d1c0
Ayansh Varma <ayanshchess@gmail.com>
2026-04-22 16:43:29 -0700

    feat: add barg graph generation and save it to a .png file

--- analysis.py
+++ analysis.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 def count_skills(data):
     skill_counts = {}
     for job in data:
@@ -8,10 +10,24 @@ def count_skills(data):
                 skill_counts[skill] = 1
     return(skill_counts)
 
-from scraper import extract, transform
+def generate_chart(skill_counts):
+    sorted_skills = sorted(skill_counts.items(), key = lambda x: x[1], reverse = True)
+    top_20 = sorted_skills [:20]
+    skills = [item[0] for item in top_20]
+    counts = [item[1] for item in top_20]
+    plt.figure(figsize=(12, 6))
+    plt.bar(skills, counts)
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.savefig('skills_chart.png')
+    plt.show()
+    print("Chart saved as skills_chart.png")
 
-jobs = extract()
-data = transform(jobs)
 
-counts = count_skills(data)
-print(counts)
+if __name__ == "__main__":
+    from scraper import extract, transform
+    jobs = extract()
+    data = transform(jobs)
+    counts = count_skills(data)
+    generate_chart(counts)
+    print(counts)
